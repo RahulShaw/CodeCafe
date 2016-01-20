62ac7bd7b08491d8e4461b22cd08987df7dceb2e
RahulShaw <therahulshaw@gmail.com>
2016-01-21 01:58:01 +0530

    Added Check For Account Activation Status

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
                 
                     $email = htmlentities(mysqli_real_escape_string($connection, trim($_POST['email'])));
                     $password = sha1(md5(htmlentities(mysqli_real_escape_string($connection, trim($_POST['password'])))));
-                    $status = 1; // Here we set by default status In-active.
+                    //$status = 1; // Here we set by default status In-active.
                 
                     if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 
@@ -32,26 +32,33 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
                             
                             $email_row = $check_email->fetch_assoc();
                             $email_address = $email_row['email'];
+                            $activation_status = $email_row['status'];
+
+                            if($activation_status == 1) {
                             
-                            $password_query = "SELECT password FROM `users` WHERE email = '{$email_address}'";
-                            $check_password_query = mysqli_query($connection, $password_query);
-                            
-                            $password_row = $check_password_query->fetch_assoc();
-                            $password_from_database = $password_row['password'];
-                            
-                            if($password === $password_from_database) {
+                                $password_query = "SELECT password FROM `users` WHERE email = '{$email_address}'";
+                                $check_password_query = mysqli_query($connection, $password_query);
                                 
-                                $_SESSION['email'] = $email;
-								$_SESSION['okayToken'] = sha1($password_from_database);
-                                $_SESSION['loginTime'] = time();
-                                //header('Location: index.php');
-                                //$data = array("result" => 1, "message" => "Credintials Validated! You Will Be Redirected!");
+                                $password_row = $check_password_query->fetch_assoc();
+                                $password_from_database = $password_row['password'];
                                 
+                                if($password === $password_from_database) {
+                                    
+                                    $_SESSION['email'] = $email;
+    								$_SESSION['okayToken'] = sha1($password_from_database);
+                                    $_SESSION['loginTime'] = time();
+                                    //header('Location: index.php');
+                                    //$data = array("result" => 1, "message" => "Credintials Validated! You Will Be Redirected!");
+                                    
+                                }
+                                else {
+                                    $data = array("result" => -7, "message" => "Wrong Password!");
+                                }
                             }
-                            else {
-                                $data = array("result" => -7, "message" => "Wrong Password!");
+                            else
+                            {
+                                $data = array("result" => -8, "message" => "Account Has Not Been Activated!");
                             }
-                            
                             
                         }
                         else {
